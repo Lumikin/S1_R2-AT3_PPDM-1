17bc605eb1321cf36255b49ac6287b1904fe53d7
Lumikin <lumikomio02@gmail.com>
2026-03-30 08:34:32 -0300

    Adicionando a pagina de contatos

--- src/screens/Contato/index.js
+++ src/screens/Contato/index.js
@@ -6,7 +6,11 @@ export default function Contato() {
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-      <Text style={styles.Text} > Telefone:  </Text>
+      <Text style={styles.Text} > Nossos contatos </Text>
+      <Text style={styles.Text} > Telefone:   +55 (11) 4002-8922</Text>
+      <Text style={styles.Text} > WhatsApp:  +55 (11) 99999-0000</Text>
+      <Text style={styles.Text} > Endereço:  Praça Dom Máximo, 420, Térreo/Loja</Text>
+      <Text style={styles.Text} > CNPJ:  00.000.000/0001-00</Text>
     </SafeAreaView>
   );
 }
@@ -21,6 +25,7 @@ const styles = StyleSheet.create({
     gap: 5
   },
   Text:{
-
+    fontWeight: 'bold',
+    fontSize: 16
   }
 });
